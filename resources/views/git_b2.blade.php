5c14b0c02c844e1e464d577c887d850aa8ec7b0c
montazerii <Montazeriput95@gmail.com>
2020-06-15 11:43:37 +0430

    b2 fisrt commit

---
+++ resources/views/git_b2.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>B2 Branch</title>
+</head>
+<body>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis ratione commodi, neque voluptatem a ad voluptas reprehenderit expedita in perferendis soluta autem deserunt perspiciatis ipsa culpa dignissimos nostrum dolore officia?
+    </p>
+</body>
+</html>
