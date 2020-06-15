ff3424839a0f7dffef62027a8210c3a31249931a
montazerii <Montazeriput95@gmail.com>
2020-06-15 11:36:05 +0430

    b1 fisrt commit

---
+++ resources/views/git.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Git Test</title>
+</head>
+<body>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe hic similique, dicta facilis laudantium iusto corrupti mollitia sint veritatis harum ipsam fugiat modi porro sit esse dolores. Laudantium, ipsam nobis?
+    </p>
+</body>
+</html>
